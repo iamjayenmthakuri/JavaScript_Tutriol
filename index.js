ca7410b8ffa28196f587ea947f0862286a3edf64
iamjayenmthakuri <malla.jayen@gmail.com>
2023-03-28 18:30:49 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// let firstName = "Jayen";
+// let secondNAme =" Malla"
+// console.log(firstName + secondNAme);
+
+// const and variable
+
+// const interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+// primitive types
+
+// let name = "Jayen"
+// let age = 23;
+// let isApproved = true;
+// let hiName = undefined;
+// let selectedColor = null;
+
+// objects
+
+// let person = {
+//     name : "Jayen",
+//     age : 23
+
+// };
+// Dot Notation
+// person.name = "Kishor"
+
+// Brackets Notation
+// person['name']="Jayen";
+// console.log(person.name);
+// console.log(person)
+
+// let selection = "name";
+// person[selection]= 'Ram';
+// console.log(person.name);
+
+
+// Arrays is an objects
+// let selectedColor = ['red','blue'];
+// selectedColor[2] = 'green';
+// selectedColor [3] = 3;
+// console.log(selectedColor);
+// console.log(selectedColor[0]);
+// console.log(selectedColor.length);
+
+// function greet(name, lastName){
+
+// console.log("Hello " + name +" "+ lastName);
+// }
+// greet('Jayen', "Malla");
+// greet('Kishor',"Malla");
+
+
+// types of functions
+// function square(number){
+//     return number *number;
+// }
+// let number = square(2);
+// console.log(number);
+// console.log(square(6));
